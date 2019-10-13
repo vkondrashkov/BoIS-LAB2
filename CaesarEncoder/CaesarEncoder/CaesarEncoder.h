b89b587a50732069c8b50fbeec6d3fdbf3b5047a
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-10-13 16:02:32 +0300

    Added Caesar encoder

---
+++ CaesarEncoder/CaesarEncoder/CaesarEncoder.h
@@ -0,0 +1,19 @@
+//
+//  CaesarEncoder.h
+//  CaesarEncoder
+//
+//  Created by Vladislav Kondrashkov on 10/13/19.
+//  Copyright © 2019 vkondrashkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CaesarEncoder.
+FOUNDATION_EXPORT double CaesarEncoderVersionNumber;
+
+//! Project version string for CaesarEncoder.
+FOUNDATION_EXPORT const unsigned char CaesarEncoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CaesarEncoder/PublicHeader.h>
+
+
