1570aaf67fc25e354cc208b8a40c8fa132fb9eca
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-10-13 16:01:02 +0300

    Added startup application

---
+++ lab2/lab2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab2
+//
+//  Created by Vladislav Kondrashkov on 10/13/19.
+//  Copyright © 2019 vkondrashkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
