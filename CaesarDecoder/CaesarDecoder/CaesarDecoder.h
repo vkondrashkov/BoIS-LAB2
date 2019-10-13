cb82cb51f71b8641715580ec3e1e3e220d9d7ca0
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-10-13 16:03:11 +0300

    Added Caesar decoder

---
+++ CaesarDecoder/CaesarDecoder/CaesarDecoder.h
@@ -0,0 +1,19 @@
+//
+//  CaesarDecoder.h
+//  CaesarDecoder
+//
+//  Created by Vladislav Kondrashkov on 10/13/19.
+//  Copyright © 2019 vkondrashkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CaesarDecoder.
+FOUNDATION_EXPORT double CaesarDecoderVersionNumber;
+
+//! Project version string for CaesarDecoder.
+FOUNDATION_EXPORT const unsigned char CaesarDecoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CaesarDecoder/PublicHeader.h>
+
+
